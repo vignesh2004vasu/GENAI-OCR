d9da973bc90dbb40b49ad715479e061bcc57ea3c
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-06-03 18:52:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+{
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["src/*"]
+    }
+  }
+}
